44f204ae1e0c0aa6bb93a23fa61a7a3a8e0649e1
taipham <taiphamhuu56ld@gmail.com>
2024-08-15 23:28:55 +0700

    [FEAT] F11: Add password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
+app.secret_key = "b2dd17eb994d430edbc07467c6805875"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
