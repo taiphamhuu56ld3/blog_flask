7f79d4fb72d57ed02282843b3fffb36694a624ce
taipham <taiphamhuu56ld@gmail.com>
2024-08-17 19:57:17 +0700

    [FEAT] F11: Add load_user function

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,10 @@
-from flaskblog import db
+from flaskblog import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
@@ -11,7 +15,7 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref = 'author', lazy  = True)
 
     def __repr__(self):
-        return f"Uer('{self.user_name}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.user_name}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
