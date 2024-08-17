ca750366bc021ae5f700835d40e6641cf5dae7ce
taipham <taiphamhuu56ld@gmail.com>
2024-08-17 19:54:37 +0700

    [FEAT] F11: Add login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,3 +1,5 @@
+
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
@@ -8,3 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
