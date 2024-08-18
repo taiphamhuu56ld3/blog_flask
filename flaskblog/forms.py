047e1ed0b83010aeb9a78d6e051743f23bd7af7e
taipham <taiphamhuu56ld@gmail.com>
2024-08-18 16:19:29 +0700

    [FEAT] F12: Create RegistrationForm based on FlaskForm

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.fields.simple import BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import (DataRequired, Email, EqualTo, Length,
+                                ValidationError)
+
+from flaskblog.models import User
+
+
+class RegistrationForm(FlaskForm):
+    user_name = StringField('Username', validators=[
+                            DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_user_name(self, user_name):
+        user = User.query.filter_by(user_name=user_name.data).first()
+        if user:
+            raise ValidationError(
+                'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
