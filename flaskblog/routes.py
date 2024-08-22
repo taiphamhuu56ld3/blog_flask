f3d8f2f633a011b941a92abaceea484bd1d0e96a
taipham <taiphamhuu56ld@gmail.com>
2024-08-22 23:22:29 +0700

    [FEAT] F12: Add image for account page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -70,7 +70,9 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file)
 
 @app.route("/post/new", methods=['GET', 'POST'])
 def new_post():
