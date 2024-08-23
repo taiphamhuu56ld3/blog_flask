b7b9df0cc402a501ddde4ce2c9d1caa8c27c22d3
taipham <taiphamhuu56ld@gmail.com>
2024-08-24 00:00:55 +0700

    [FEAT] F12: Add function update and delete post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import flash, render_template, url_for
+from flask import abort, flash, render_template, url_for
 from flask.globals import request
 from flask_dance.contrib.github import github
 from flask_login import current_user, login_required, login_user, logout_user
@@ -143,3 +143,34 @@ def gitlogin():
     assert resp.ok
     print(resp.json())
     return "You are @{login} on GitHub".format(login=resp.json()["login"])
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Post updated!", 'success')
+        return redirect(url_for('post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, name="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete", methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post deleted!", 'success')
+    return redirect(url_for('home'))
