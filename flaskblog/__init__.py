7ac325209b2f68ae1b83cfa2c4889028863ede8c
taipham <taiphamhuu56ld@gmail.com>
2024-08-29 01:37:21 +0700

    [FEAT] F13: Functionalize create_app for reuse

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -11,22 +11,22 @@ app = Flask(__name__)
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
-
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category ='info'
 mail = Mail()
 
-app.config.from_object(Config)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
-bcrypt.init_app(app)
-login_manager.init_app(app)
-mail.init_app(app)
-
-from flaskblog import routes
-blueprint = make_github_blueprint(
-    client_id="Ov23liCBryBDxvtnwTqH",
-    client_secret="0f23236da39e20bd27b6669d12c4808d75b44c7f",
-)
-app.register_blueprint(blueprint, url_prefix="/login")
+def create_app(config_class=Config):
+    app.config.from_object(config_class)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    from flaskblog import routes
+    blueprint = make_github_blueprint(
+        client_id="Ov23liCBryBDxvtnwTqH",
+        client_secret="0f23236da39e20bd27b6669d12c4808d75b44c7f",
+    )
+    app.register_blueprint(blueprint, url_prefix="/login")
+    return app
