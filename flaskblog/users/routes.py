4ee5b705a96ea73276dc4f09557d1ab49bb9f07d
taipham <taiphamhuu56ld3@gmail.com>
2025-03-05 16:01:39 +0700

    [FIX] F13: Fix account

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -81,7 +81,7 @@ def account():
         current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
+        return redirect(url_for('users.account'))
 
     elif request.method == 'GET':
         form.username.data = current_user.user_name
