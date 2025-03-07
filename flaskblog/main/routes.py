4769f4bd0ca891f91479aec489a5485eb649e2ba
taipham <taiphamhuu56ld3@gmail.com>
2025-03-08 00:46:10 +0700

    [FEAT] F13: Divide each page into 5 posts for home

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from flaskblog.models import Post
 
@@ -8,10 +8,10 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    posts = Post.query.all()
-    return render_template('home.html', posts=posts)
-
+    page = request.args.get('page', 1, type = int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
+    return render_template('home.html', posts  = posts)
 
 @main.route("/about")
 def about():
-    return render_template('about.html', title="About")
+    return render_template('about.html', title = "About")
