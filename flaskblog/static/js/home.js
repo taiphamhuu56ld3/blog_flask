269761505953ee5c26ad21eb84866591ddde056a
taipham <taiphamhuu56ld3@gmail.com>
2025-03-15 23:26:47 +0700

    [FEAT] F14: Create loading spinner

---
+++ flaskblog/static/js/home.js
@@ -0,0 +1,5 @@
+// Handle Loading Effect
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loading-spinner").style.display = "none";
+    document.getElementById("post-container").style.display = "block";
+});
